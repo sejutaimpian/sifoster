a09ac392d3176ee1033ba1867f86531c8e9dbdff
sejutaimpian <derissulistina@gmail.com>
2022-04-09 06:51:06 +0700

    add: section gallery & modal image

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -78,6 +78,8 @@
         </div>
     </div>
 </div>
+
+<!-- Informasi -->
 <div class="container my-3">
     <div class="row bg-eris-susu border rounded-top">
         <div class="col-sm-12 my-1">
@@ -134,6 +136,66 @@
                     <p class="card-text description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, inventore atque et sint maxime debitis esse numquam suscipit enim aliquam.</p>
                     <a href="#" class="btn btn-outline-primary">Read More</a>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Gallery -->
+<div class="container my-2">
+    <div class="row bg-eris-susu border rounded-top rounded-bottom mb-2">
+        <div class="col-sm-12 my-1">
+            <div class="row d-flex align-items-center">
+                <div class="col-10">
+                    <i class="fa-solid fa-images fs-2"></i>
+                    <span class="h1 eris-hidden">Gallery</span>
+                </div>
+                <div class="col-2 text-end">
+                    <a href="/gallery" class="btn">
+                        <i class="fa-solid fa-arrow-right fs-2"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="row g-2 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+        <div class="col">
+            <img src="/image/team.jpg" class="gallery-item img-thumbnail">
+        </div>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="gallery-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="/image/team.jpg" class="modal-img gallery-item" alt="">
             </div>
         </div>
     </div>
--- public/script.js
+++ public/script.js
@@ -21,3 +21,14 @@ function previewImg(){
 }
 
 // ckeditor
+
+
+// Modal Gallery
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src= src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
