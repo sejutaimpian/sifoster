83374270d963430ef5c0035b6c258df698cef657
sejutaimpian <derissulistina@gmail.com>
2022-04-09 16:11:48 +0700

    add: layout halaman kurikulum

--- app/Views/pages/kurikulum.php
+++ app/Views/pages/kurikulum.php
@@ -1,11 +1,55 @@
 <?= $this->extend("layout/template"); ?>
 
 <?= $this->section("content"); ?>
-<div class="container">
+<div class="container my-2">
     <div class="row">
-        <h1><?= $title; ?></h1>
-        <div class="col-md-12">
-            <h2>Kurikulum 1</h2>
+        <div class="col-md-9">
+            <div id="pendahuluan">
+                <h2>Pendahuluan</h2>
+                <p>Kurikulum ini hadir untuk memberikan kepastian materi kepada para calon pendaftar sebagai patokan materi apa saja yang akan dipelajari nanti di sekolah</p>
+                <p>Kurikulum ini memuat pengklasifikasian siswa didik, penilaian, dan tata tertib siswa</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo expedita eos possimus, asperiores fugiat aperiam ut molestiae pariatur vel vero, perspiciatis dolorem, quas voluptatem. Quibusdam voluptatem in atque aspernatur?</p>
+            </div>
+            <div id="klasifikasi">
+                <h2>Klasifikasi / Kelompok Belajar</h2>
+                <p>Kurikulum ini hadir untuk memberikan kepastian materi kepada para calon pendaftar sebagai patokan materi apa saja yang akan dipelajari nanti di sekolah</p>
+                <p>Kurikulum ini memuat pengklasifikasian siswa didik, penilaian, dan tata tertib siswa</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo expedita eos possimus, asperiores fugiat aperiam ut molestiae pariatur vel vero, perspiciatis dolorem, quas voluptatem. Quibusdam voluptatem in atque aspernatur?</p>
+            </div>
+            <div id="penilaian">
+                <h2>Penilaian Siswa</h2>
+                <p>Kurikulum ini hadir untuk memberikan kepastian materi kepada para calon pendaftar sebagai patokan materi apa saja yang akan dipelajari nanti di sekolah</p>
+                <p>Kurikulum ini memuat pengklasifikasian siswa didik, penilaian, dan tata tertib siswa</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo expedita eos possimus, asperiores fugiat aperiam ut molestiae pariatur vel vero, perspiciatis dolorem, quas voluptatem. Quibusdam voluptatem in atque aspernatur?</p>
+            </div>
+            <div id="tatatertib">
+                <h2>Tata Tertib Siswa</h2>
+                <p>Kurikulum ini hadir untuk memberikan kepastian materi kepada para calon pendaftar sebagai patokan materi apa saja yang akan dipelajari nanti di sekolah</p>
+                <p>Kurikulum ini memuat pengklasifikasian siswa didik, penilaian, dan tata tertib siswa</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo expedita eos possimus, asperiores fugiat aperiam ut molestiae pariatur vel vero, perspiciatis dolorem, quas voluptatem. Quibusdam voluptatem in atque aspernatur?</p>
+            </div>
+        </div>
+        <div class="col-md-3 eris-hidden">
+            <div>
+                <h5>Daftar Isi</h5>
+                <ol>
+                    <a href="#pendahuluan">
+                        <li>Pendahuluan</li>
+                    </a>
+                    <a href="#klasifikasi">
+                        <li>Klasifikasi / Kelompok</li>
+                    </a>
+                    <a href="#penilaian">
+                        <li>Penilaian</li>
+                    </a>
+                    <a href="#tatatertib">
+                        <li>Tata Tertib Siswa</li>
+                    </a>
+                </ol>
+            </div>
+            <div>
+                <h5>Lorem</h5>
+            </div>
         </div>
     </div>
 </div>
