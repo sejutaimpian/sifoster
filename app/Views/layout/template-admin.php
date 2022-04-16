a1cc1faf98055f3c26c6597a534e845f08a0a9b9
sejutaimpian <derissulistina@gmail.com>
2022-04-16 13:27:58 +0700

    fix: navbar admin

--- app/Views/layout/template-admin.php
+++ app/Views/layout/template-admin.php
@@ -89,15 +89,23 @@
                             </div>
                         </div>
                         <div class="">
-                            <a class="nav-link" href="/admin/profileorganisasi">
-                                <div class="sb-nav-link-icon"><i class="fa-solid fa-book"></i></div>
-                                Profil Organisasi
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePengaturan" aria-expanded="false" aria-controls="collapsePengaturan">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-gear"></i></div>
+                                Pengaturan
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
+                            <div class="collapse" id="collapsePengaturan" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="/admin/profileorganisasi">Profile Organisasi</a>
+                                    <a class="nav-link" href="/admin/kategori">Kategori</a>
+                                    <a class="nav-link" href="/admin/manajemenakun">Manajemen Akun</a>
+                                </nav>
+                            </div>
                         </div>
                         <div class="">
-                            <a class="nav-link" href="/admin/manajemenakun">
-                                <div class="sb-nav-link-icon"><i class="fa-solid fa-computer-mouse"></i></div>
-                                Manajemen Akun
+                            <a class="nav-link" href="/logout">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-right-from-bracket"></i></div>
+                                Logout
                             </a>
                         </div>
                     </div>
